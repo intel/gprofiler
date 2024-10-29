bf4b7b1e3274f1e3c6aa5a1c246f53fbdbaa231a
slicklash <slicklash@gmail.com>
2024-10-29 17:04:20 +0100

    up

--- scripts/perf_build.sh
+++ scripts/perf_build.sh
@@ -17,7 +17,7 @@
 set -euo pipefail
 
 # downloading the zip because the git is very large (this is also very large, but still smaller)
-curl -SL https://codeload.github.com/Granulate/linux/zip/169b16658825031b20e32b34c5aedc22ed093abc -o linux.zip
+curl -SL https://codeload.github.com/Granulate/linux/zip/50259d0151087e75b8bfd6c0b0edc3125ee9997f -o linux.zip
 unzip -qq linux.zip
 rm linux.zip
 cd linux-*/
