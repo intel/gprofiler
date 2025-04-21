8701b98789dff8ee86a6cc3f2c2f31aef9f74b1b
Ron Sobol <ron.sobol@granulate.io>
2025-04-21 23:45:36 +0300

    Update bpf_get_stack_offset with fix

--- scripts/bcc_helpers_build.sh
+++ scripts/bcc_helpers_build.sh
@@ -37,6 +37,6 @@ cd /bpf_get_fs_offset && git reset --hard 8326d39cf44845d4b643ed4267994afca8ccec
 cd /bpf_get_fs_offset && make $LIBBPF_MAKE_FLAGS
 
 cd / && git clone -b feature/add-support-for-kernel-6.9 --depth=1 --recurse-submodules https://github.com/Granulate/bpf_get_stack_offset.git
-cd /bpf_get_stack_offset && git reset --hard 56a133ec8b873d787cb66afb21e629f7f8ed619d
+cd /bpf_get_stack_offset && git reset --hard c5526e706c36efea9e2f75ceeb603636cf386ec7
 # shellcheck disable=SC2086
 cd /bpf_get_stack_offset && make $LIBBPF_MAKE_FLAGS
